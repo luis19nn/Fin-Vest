fa90c47432695910d7ab53a7adc5d991eab004f7
luis19nn <luis19nn@gmail.com>
2022-07-01 11:26:13 -0300

    ADD estrutura inicial da aplicação

--- src/main/java/com/finvest/App.java
+++ src/main/java/com/finvest/App.java
@@ -1,10 +1,49 @@
 package com.finvest;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import utils.Helper;
+
 public final class App {
-    private App() {
-    }
+
+    /**
+     * MAIN
+     */
 
     public static void main(String[] args) {
-
     }
+
+    /**
+     * MENU
+     */
+
+    public static void menu() {
+    }
+
+    /**
+     * MÉTODOS DE RENTABILIDADE
+     */
+
+    public static double valorFinalCapitalInvestido(double valorCapital, int prazoInvestimento, double taxaJuros) {
+    }
+
+    public static double[][] resultadoMesAMes(double valorCapital, int prazoInvestimento, double taxaJuros) {
+    }
+
+    public static double taxaJurosValorFinal(double valorCapital, int prazoInvestimento, double taxaJuros) {
+    }
+
+    /**
+     * VIEWS PARA OS MÉTODOS DE RENTABILIDADE
+     */
+
+    public static void valorFinalCapitalInvestidoView(double valorCapital, int prazoInvestimento, double taxaJuros) {
+    }
+
+    public static void resultadoMesAMesView(double valorCapital, int prazoInvestimento, double taxaJuros) {
+    }
+
+    public static void taxaJurosValorFinalView(double valorCapital, int prazoInvestimento, double taxaJuros) {
+    }
+
 }
