e3b969838a2b82e39fab479fee94435c1c37658d
luis19nn <luis19nn@gmail.com>
2022-07-01 10:40:44 -0300

    INIT o projeto Fin-Vest

---
+++ src/main/java/com/finvest/App.java
@@ -0,0 +1,10 @@
+package com.finvest;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/finvest/AppTest.java
@@ -0,0 +1,11 @@
+package com.finvest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
