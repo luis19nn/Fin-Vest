36c965af25a086e9c319fc0d45dcec2a4ef63c9d
luis19nn <luis19nn@gmail.com>
2022-07-01 11:17:59 -0300

    ADD função que ajuda na utilização do Scanner

--- src/main/java/utils/Helper.java
+++ src/main/java/utils/Helper.java
@@ -1,5 +1,13 @@
 package utils;
 
+import java.util.Scanner;
+
 public class Helper {
-    
+
+    public static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+    }
+
 }
