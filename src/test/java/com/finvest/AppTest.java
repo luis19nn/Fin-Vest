4872d45b069206bdae4cd58c791fa879573162bb
luis19nn <luis19nn@gmail.com>
2022-07-01 10:52:22 -0300

    ADD estrutura para os testes dos métodos que serão feitos

--- src/test/java/com/finvest/AppTest.java
+++ src/test/java/com/finvest/AppTest.java
@@ -2,10 +2,42 @@ package com.finvest;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.InputMismatchException;
 
 class AppTest {
+
+    /**
+     * CAMINHO FELIZ
+     */
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testValorFinalCapitalInvestido1() {
     }
+
+    @Test
+    void testResultadoMesAMes1() {
+    }
+
+    @Test
+    void testTaxaJurosValorFinal1() {
+    }
+
+    /**
+     * CAMINHO INFELIZ
+     */
+
+    @Test
+    void testValorFinalCapitalInvestido2() {
+    }
+
+    @Test
+    void testResultadoMesAMes2() {
+    }
+
+    @Test
+    void testTaxaJurosValorFinal2() {
+    }
+
 }
