f61ec9dbb4332f2f175413eaa38784a839854b65
luis19nn <luis19nn@gmail.com>
2022-07-01 11:08:53 -0300

    ADD testes para o caminho feliz do sistema, verificando se os métodos funcionam. Inicialmente feitos para falharem, seguindo o método TDD, para, em seguida, implementar os métodos testados realmente

--- src/test/java/com/finvest/AppTest.java
+++ src/test/java/com/finvest/AppTest.java
@@ -14,14 +14,37 @@ class AppTest {
 
     @Test
     void testValorFinalCapitalInvestido1() {
+        double valorCapital = 1000.0, taxaJuros = 0.1;
+        int prazoInvestimento = 6;
+
+        double expected = 1771.56;
+        double actual = App.valorFinalCapitalInvestido(valorCapital, prazoInvestimento, taxaJuros);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     void testResultadoMesAMes1() {
+        double valorCapital = 1000.0, taxaJuros = 0.1;
+        int prazoInvestimento = 6;
+
+        double[][] expected = { { 1100.0, 100.0 }, { 1210.0, 110.0 }, { 1331.0, 121.0 }, { 1464.1, 133.1 },
+                { 1610.51, 146.41 }, { 1771.56, 161.05 } };
+
+        double[][] actual = App.resultadoMesAMes(valorCapital, prazoInvestimento, taxaJuros);
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void testTaxaJurosValorFinal1() {
+        double valorCapital = 1000.0, taxaJuros = 0.1;
+        int prazoInvestimento = 6;
+
+        double expected = 161.05;
+        double actual = App.taxaJurosValorFinal(valorCapital, prazoInvestimento, taxaJuros);
+
+        assertEquals(expected, actual);
     }
 
     /**
